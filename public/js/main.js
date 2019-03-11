e027778ae6a6756c5231866163af331e47099774
Tony Fite <koopastrong@gmail.com>
2019-03-11 15:59:26 -0400

    UnRefactored initial git

---
+++ public/js/main.js
@@ -0,0 +1,40 @@
+import {loadLevel} from './loaders.js';
+import {loadMarioSprite, loadBackgroundSprites} from './sprites.js'
+import Compositor from './Compositor.js';
+import{createBackgroundLayer} from './layers.js';
+
+const canvas = document.getElementById('screen');
+const context = canvas.getContext('2d');
+
+function createSpriteLayer(sprite, pos) {
+  return function drawSpriteLayer(context) {
+    sprite.draw('idle', context, pos.x, pos.y);
+  };
+}
+
+Promise.all([
+  loadMarioSprite(),
+  loadBackgroundSprites(),
+  loadLevel('1-1'),
+])
+.then(([marioSprite, backgroundSprites, level,]) =>{
+  const comp = new Compositor();
+
+  const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
+  comp.layers.push(backgroundLayer);
+  const pos = {
+    x: 64,
+    y: 64,
+  };
+
+const spriteLayer = createSpriteLayer(marioSprite, pos);
+comp.layers.push(spriteLayer);
+
+function update() {
+  comp.draw(context);
+  pos.x+=2;
+  pos.y+=2;
+  requestAnimationFrame(update);
+  }
+  update();
+});
