187360b847b2b7a67ea502a49f3e8e4c3ac8773d
Tony Fite <koopastrong@gmail.com>
2019-03-22 00:14:34 -0400

    Fixed some css and added banners

---
+++ public/public/js/main.js
@@ -0,0 +1,42 @@
+import Timer from './Timer.js';
+import {loadLevel} from './loaders.js';
+import {createMario} from './entities.js';
+
+const canvas = document.getElementById('screen');
+const context = canvas.getContext('2d');
+
+import Keyboard from './KeyboardStates.js';
+
+Promise.all([
+    createMario(),
+    loadLevel('1-1'),
+])
+.then(([mario, level]) => {
+
+
+    const gravity = 2000;
+    mario.pos.set(64, 64);
+
+    level.entities.add(mario);
+
+    const SPACE = 32;
+    const input = new Keyboard();
+    input.addMapping(SPACE, keyState => {
+      if(keyState) {
+        mario.jump.start();
+      }else{
+        mario.jump.cancel();
+      }
+      console.log("Space Key Pressed!");
+    });
+    input.listenTo(window);
+
+    const timer = new Timer(1/60);
+    timer.update = function update(deltaTime) {
+        level.update(deltaTime);
+        level.comp.draw(context);
+        mario.vel.y += gravity * deltaTime;
+    }
+
+    timer.start();
+});
